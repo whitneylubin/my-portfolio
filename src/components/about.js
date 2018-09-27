6f7ba9f1ab8ffbc70132b22b27402f749fd93708
Whitney Lubin <whitneylubin@icloud.com>
2018-09-27 14:18:41 -0400

    about section update

--- src/components/about.js
+++ src/components/about.js
@@ -10,7 +10,17 @@ const AboutSection = () => (
             <h2>I'm your next Full Stack Web Developing Ninja!</h2>
             <p className="lead mb-5">
               My French and my Portugeuse could use some work, but I speak
-              fluent Javascript and Ruby.
+              fluent Javascript and Ruby. I'm obsessed with all the varied ways
+              in which technology can be used to improve lives, and increase
+              efficiency. Outside of coding you'll usually find me doing
+              something in my community to make it a better place to work,
+              learn, and live. I like working with companies that allow me to be
+              an Ambassador for the tools that I have a part in building. No
+              matter where it is I'm working I always see myself as a business
+              partner in helping make sure that mission of the organization and
+              my team is accomplished. If you are looking for someone to add to
+              your team who is a community evanglist, entrepreneurial, and full
+              of team spirit, than I'm your techie.
             </p>
             <a
               className="btn btn-dark btn-xl js-scroll-trigger"
